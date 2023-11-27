daf1e3317324ecc131d64071accb95d5d469758d
rohithvangapalli <106822930+rohithvangapalli@users.noreply.github.com>
2023-11-27 20:14:55 +0530

    Update jar_udf.scala

--- jar_udf.scala
+++ jar_udf.scala
@@ -2,14 +2,8 @@ import org.apache.spark.sql.SparkSession
 import DataFrameProcessor.processDataFrame
  
 val spark: SparkSession = SparkSession.builder().appName("AlmarenEMR").enableHiveSupport().getOrCreate()
- 
-var df = spark.read
-  .format("csv")
-  .option("header", "true")
-  .option("inferSchema", "false")
-  .option("delimiter", "\t")
-  .option("comment", "#")
-  .load("s3://gratis-bucket-test/mt4002_testing/sample.gz")
+
+var df = spark.read.format("csv").option("header", "true").option("inferSchema", "false").option("delimiter", "\t").option("comment", "#").load("s3://gratis-bucket-test/mt4002_testing/sample.csv.gz")
  
 df = DataFrameProcessor.processDataFrame(df)
  
