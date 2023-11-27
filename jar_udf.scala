f009ef7f408f24d942fca9eacb99d6284dff40f9
rohithvangapalli <106822930+rohithvangapalli@users.noreply.github.com>
2023-11-27 20:01:11 +0530

    Create jar_udf.scala

---
+++ jar_udf.scala
@@ -0,0 +1,16 @@
+import org.apache.spark.sql.SparkSession
+import DataFrameProcessor.processDataFrame
+ 
+val spark: SparkSession = SparkSession.builder().appName("AlmarenEMR").enableHiveSupport().getOrCreate()
+ 
+var df = spark.read
+  .format("csv")
+  .option("header", "true")
+  .option("inferSchema", "false")
+  .option("delimiter", "\t")
+  .option("comment", "#")
+  .load("s3a://gratis-bucket-test/mt4002_testing/sample.gz")
+ 
+df = DataFrameProcessor.processDataFrame(df)
+ 
+df.show(false)
