4ec6228bac85b06ee91782d1fe0c1d305140e4fd
rohithvangapalli <106822930+rohithvangapalli@users.noreply.github.com>
2023-12-04 19:23:39 +0530

    Update hadoop.scala

--- hadoop.scala
+++ hadoop.scala
@@ -1,4 +1,11 @@
 import org.apache.hadoop.util.VersionInfo
- 
-val hadoopVersion = VersionInfo.getVersion
-println(s"Hadoop version: $hadoopVersion")
+
+try {
+  val hadoopVersion = VersionInfo.getVersion
+  println(s"Hadoop version: $hadoopVersion")
+  sys.exit(0) // Exit with success status
+} catch {
+  case e: Throwable =>
+    println(s"Error getting Hadoop version: ${e.getMessage}")
+    sys.exit(1) // Exit with error status
+}
